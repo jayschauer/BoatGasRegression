2c8b42a7cb8a1f64db5a3a721740c71db59dea92
Jay Schauer <jschauer@umich.edu>
2023-06-07 16:06:35 -0400

    Change to estimate gallons instead of fuel gauge reading

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 A linear regression to estimate how much boat gas the umich waterski team uses each time we ski.
 
 Original data is recorded here: https://docs.google.com/spreadsheets/d/1wlKaxpww5Z7icWqO8fdrtR49Uo22-nshw6ypt9hyWGg/edit?usp=sharing
+
+I don't really know anything about data science so take this with a grain of salt.
--- regression.py
+++ regression.py
@@ -6,11 +6,17 @@ from sklearn.linear_model import ElasticNetCV
 # Setup models
 # For the model, include the Y-intercept and force positive coefficients
 linear_regressor = LinearRegression(fit_intercept=True, positive=True)
-# same as ridgecv when l1_ratio=0
+# Elastic is same as Ridge when l1_ratio=0
+ridge_regressor = ElasticNetCV(fit_intercept=True, positive=True,
+                               tol=1e-5, max_iter=10000,
+                               l1_ratio=0, alphas=np.arange(0.001, 0.1, 0.001))
 elastic_regressor = ElasticNetCV(fit_intercept=True, positive=True,
-                                 tol=1e-5, max_iter=10000,
-                                 l1_ratio=0, alphas=np.arange(0.001, 0.1, 0.001))
+                                 l1_ratio=np.arange(0.01, 0.3, 0.01),
+                                 tol=1e-5, max_iter=10000)
+
 # Setup variables and boat parameters
+# See google sheet document:
+# https://docs.google.com/spreadsheets/d/1wlKaxpww5Z7icWqO8fdrtR49Uo22-nshw6ypt9hyWGg/edit?usp=sharing
 fuel_gauge_range = 16.0  # fuel gauge range in gallons
 variables = ['Trick Sets', 'Slalom Sets <= 30mph', 'Slalom Sets > 30mph', 'Jump Sets <= 28mph', 'Jump Sets > 28mph']
 # Ski sets to estimate gas for, same order as variables:
@@ -18,25 +24,37 @@ to_predict = np.array([[4, 2, 2, 0, 0]])
 
 # Setup Data
 df = pd.read_csv('data.csv', sep=',', header=0)
-df['Change'] = df['Starting Reading'] - df['Ending Reading']
-y = df['Change'].values
+df['Change'] = df['Start Reading'] - df['End Reading']
+df['Gallons'] = df['Change']*fuel_gauge_range
+y = df['Gallons'].values
 X = df[variables].values
 sample_weights = df['Weight'].values
 
 # Make models
-linear_model_weighted = linear_regressor.fit(X, y, sample_weights)
-elastic_model_weighted = elastic_regressor.fit(X, y, sample_weights)
+# TODO: make models without weights once we have more data
+linear_model = linear_regressor.fit(X, y, sample_weights)
+elastic_model = elastic_regressor.fit(X, y, sample_weights)
+ridge_model = ridge_regressor.fit(X, y, sample_weights)
 
-print('Linear weighted coefficients, intercept, R^2, prediction:')
-print(linear_model_weighted.coef_)
-print(linear_model_weighted.intercept_)
-print(linear_model_weighted.score(X, y, sample_weights))
-print(linear_model_weighted.predict(to_predict) * fuel_gauge_range)
-
-print('Elastic weighted coefficients, intercept, l1 ratio, alpha, R^2, prediction:')
-print(elastic_model_weighted.coef_)
-print(elastic_model_weighted.intercept_)
-print(elastic_model_weighted.l1_ratio_)
-print(elastic_model_weighted.alpha_)
-print(elastic_model_weighted.score(X, y, sample_weights))
-print(elastic_model_weighted.predict(to_predict) * fuel_gauge_range)
+print(f"Average gallons of gas used: {pd.Series.mean(df['Gallons'])}")
+print()
+print('Linear model:')
+print(f'coefficients: {linear_model.coef_}')
+print(f'intercept: {linear_model.intercept_}')
+print(f'R^2: {linear_model.score(X, y, sample_weights)}')
+print(f'prediction: {linear_model.predict(to_predict)}')
+print()
+print('Ridge model:')
+print(f'coefficients: {ridge_model.coef_}')
+print(f'intercept: {ridge_model.intercept_}')
+print(f'alpha: {ridge_model.alpha_}')
+print(f'R^2: {ridge_model.score(X, y, sample_weights)}')
+print(f'prediction: {ridge_model.predict(to_predict)}')
+print()
+print('Elastic model:')
+print(f'coefficients: {elastic_model.coef_}')
+print(f'intercept: {elastic_model.intercept_}')
+print(f'l1 ratio: {elastic_model.l1_ratio_}')
+print(f'alpha: {elastic_model.alpha_}')
+print(f'R^2: {elastic_model.score(X, y, sample_weights)}')
+print(f'prediction: {elastic_model.predict(to_predict)}')
