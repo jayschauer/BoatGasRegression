a71f2844bf49edd0cdf35a361181c0d606d93b9e
Jay Schauer <jschauer@umich.edu>
2023-06-09 21:37:57 -0400

    got more data, removed sample weights

--- regression.py
+++ regression.py
@@ -9,9 +9,9 @@ linear_regressor = LinearRegression(fit_intercept=True, positive=True)
 # Elastic is same as Ridge when l1_ratio=0
 ridge_regressor = ElasticNetCV(fit_intercept=True, positive=True,
                                tol=1e-5, max_iter=10000,
-                               l1_ratio=0, alphas=np.arange(0.001, 0.1, 0.001))
+                               l1_ratio=0, alphas=np.arange(0.0001, 0.01, 0.0001))
 elastic_regressor = ElasticNetCV(fit_intercept=True, positive=True,
-                                 l1_ratio=np.arange(0.01, 0.3, 0.01),
+                                 l1_ratio=np.arange(0.001, 1.0, 0.001),
                                  tol=1e-5, max_iter=10000)
 
 # Setup variables and boat parameters
@@ -28,27 +28,26 @@ df['Change'] = df['Start Reading'] - df['End Reading']
 df['Gallons'] = df['Change']*fuel_gauge_range
 y = df['Gallons'].values
 X = df[variables].values
-sample_weights = df['Weight'].values
 
 # Make models
-# TODO: make models without weights once we have more data
-linear_model = linear_regressor.fit(X, y, sample_weights)
-elastic_model = elastic_regressor.fit(X, y, sample_weights)
-ridge_model = ridge_regressor.fit(X, y, sample_weights)
+linear_model = linear_regressor.fit(X, y)
+ridge_model = ridge_regressor.fit(X, y)
+elastic_model = elastic_regressor.fit(X, y)
 
 print(f"Average gallons of gas used: {pd.Series.mean(df['Gallons'])}")
 print()
 print('Linear model:')
 print(f'coefficients: {linear_model.coef_}')
 print(f'intercept: {linear_model.intercept_}')
-print(f'R^2: {linear_model.score(X, y, sample_weights)}')
+print(f'R^2: {linear_model.score(X, y)}')
 print(f'prediction: {linear_model.predict(to_predict)}')
 print()
 print('Ridge model:')
 print(f'coefficients: {ridge_model.coef_}')
 print(f'intercept: {ridge_model.intercept_}')
+print(f'l1 ratio: {ridge_model.l1_ratio_}')
 print(f'alpha: {ridge_model.alpha_}')
-print(f'R^2: {ridge_model.score(X, y, sample_weights)}')
+print(f'R^2: {ridge_model.score(X, y)}')
 print(f'prediction: {ridge_model.predict(to_predict)}')
 print()
 print('Elastic model:')
@@ -56,5 +55,5 @@ print(f'coefficients: {elastic_model.coef_}')
 print(f'intercept: {elastic_model.intercept_}')
 print(f'l1 ratio: {elastic_model.l1_ratio_}')
 print(f'alpha: {elastic_model.alpha_}')
-print(f'R^2: {elastic_model.score(X, y, sample_weights)}')
+print(f'R^2: {elastic_model.score(X, y)}')
 print(f'prediction: {elastic_model.predict(to_predict)}')
